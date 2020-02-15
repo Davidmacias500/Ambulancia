786ca0d22716abdc521bbbfea2abd29fbe76e0f7
David Macías <51835013+Davidmacias500@users.noreply.github.com>
2020-02-15 12:23:52 -0600

    Se agrega el destino en el modelo

--- app/Solicitud.php
+++ app/Solicitud.php
@@ -9,7 +9,7 @@ class Solicitud extends Model
     //
     protected $table='solicitudes';
     protected $primaryKey='id_solicitud';
-    protected $with=['usuario'];
+    protected $with=['usuario','destino'];
 
     public $timestamps=true;
     public $incrementing=true;
@@ -19,11 +19,16 @@ class Solicitud extends Model
         'hora_solicitud',
     	'fecha_uso',
         'hora_uso',
+        'id_destino',
     	'estatus_solicitud',
     ];
 
     public function usuario()
     {
         return $this->belongsTo(Usuario::class, 'curp','curp');
+    }
+    public function destino()
+    {
+        return $this->belongsTo(Destino::class, 'id_destino','id_destino');
     }
 }
